c7cc407c2a3900a98f62547eacfefb5ad38e14fe
stevenfleming <12flemings@gmail.com>
2020-03-07 14:47:34 -0800

    testing logic for creating an array of words for an inputed sentence

--- WordCounter.Tests/ModelTests/ClassNameTests.cs
+++ WordCounter.Tests/ModelTests/ClassNameTests.cs
@@ -4,13 +4,20 @@ using WordCounter;
 namespace Project.Tests
 {
   [TestClass]
-  public class WordCounterTests
+  public class RepeateWordCounterTests
   {
     [TestMethod]
-    public void RepeatWordCounterConstructor_TakesAStringAsParameterAndReturnsItLowerCase_RepeatCounter()
+    public void RepeatWordCounterConstructor_TakesAStringAsParameterAndReturnsItLowerCase_RepeatWordCounter()
     {
       RepeatWordCounter newCounter = new RepeatWordCounter("Hello", "Hello There");
       Assert.AreEqual(newCounter.Word, "hello");
+    }
+
+    [TestMethod]
+    public void RepeatWordCounter_TakesSentenceAndMakesAnArray_RepeatWordCounter()
+    {
+      RepeatWordCounter newCounter = new RepeatWordCounter("Hello", "Hello There");
+      Assert.AreEqual(newCounter.SentenceArrayCreator(), "hello");
     }
 
   }
